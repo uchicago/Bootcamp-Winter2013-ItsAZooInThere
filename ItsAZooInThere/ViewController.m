080a36accd11d290c797ff4de9bcf3d15d3b5937
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-05 01:17:44 -0600

    Add the animal data and debugging log method to view controller

--- ItsAZooInThere/ViewController.m
+++ ItsAZooInThere/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Animal.h"
 
 @interface ViewController ()
 
@@ -17,13 +18,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create array to hold our animals
+    _animals = [[NSMutableArray alloc] init];
+    
+    // Add new animal object - Pig
+    Animal *pig = [[Animal alloc] init];
+    pig.name = @"Pig";
+    pig.age = @5;
+    
+    // Add new animal object - Dog
+    Animal *dog = [[Animal alloc] init];
+    dog.name = @"Dog";
+    dog.age = @7;
+    
+    // Add new animal object - Narwhal
+    Animal *narwhal = [[Animal alloc] init];
+    narwhal.name = @"Narwhal";
+    narwhal.age = @20;
+    
+    // Add the animal objects to the animal array
+    [self.animals addObject:pig];
+    [self.animals addObject:dog];
+    [self.animals addObject:narwhal];
+    
+    NSLog(@"The animals in our zoo (array): %@", self.animals);
+    
+    [self logAnimals];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Debugging
+
+/*******************************************************************************
+ * @method          logAnimals
+ * @abstract        Log all animals to the console
+ * @description      
+ ******************************************************************************/
+- (void)logAnimals
+{
+    for (Animal *currentAnimal in self.animals) {
+        
+        // Call summary method in Animal class
+        [currentAnimal summary];
+    }
 }
 
 @end
