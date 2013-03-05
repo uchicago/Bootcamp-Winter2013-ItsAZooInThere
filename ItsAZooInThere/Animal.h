1fe0f64749a4184d29467a6ad1289968be7ed978
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-05 01:12:44 -0600

    Add Animal class

---
+++ ItsAZooInThere/Animal.h
@@ -0,0 +1,13 @@
+//
+//  Animal.h
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@end
---
+++ ItsAZooInThere/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
