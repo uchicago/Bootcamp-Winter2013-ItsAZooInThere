9d9e17c6216bc267e3e89941e2298a3f0cacb867
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-05 16:53:03 -0600

    Add DetailViewController

---
+++ ItsAZooInThere/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Animal.h"
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) Animal *animal;
+
+@end
---
+++ ItsAZooInThere/DetailViewController.m
@@ -0,0 +1,50 @@
+//
+//  DetailViewController.m
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import "DetailViewController.h"
+#import "Animal.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+/*******************************************************************************
+ * @method          viewDidLoad
+ * @abstract
+ * @description
+ ******************************************************************************/
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    NSLog(@">>>>>> viewDidLoad");
+}
+
+/*******************************************************************************
+ * @method          viewWillAppear:
+ * @abstract         
+ * @description
+ ******************************************************************************/
+- (void)viewWillAppear:(BOOL)animated {
+    
+    NSLog(@">>>>>> viewWillAppear");
+
+    // Print summary of animal to console for debugging
+    [self.animal summary];
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
