79c3f707cdb570e7c16aead4bc326c55cbc26bfa
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-05 00:03:19 -0600

    Initial Commit

---
+++ ItsAZooInThere/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ItsAZooInThere/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ItsAZooInThere
+//
+//  Created by T. Andrew Binkowski on 3/5/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
